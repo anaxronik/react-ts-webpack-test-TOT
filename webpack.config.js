9a280971909fbaa169f31a09fcd910bd5d14a3a2
Sergey Komarov <anaxronik@gmail.com>
2021-10-20 19:49:15 +0500

    create prjoject

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: [/\.jsx?$/, /\.tsx?$/],
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  devServer: {
+    contentBase: './dist',
+    port: 3000,
+    historyApiFallback: true,
+  },
+  plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })],
+}
