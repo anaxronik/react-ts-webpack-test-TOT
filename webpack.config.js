163ab047c663aa5f25fcc79520b7ce629b73d9cf
Sergey Komarov <anaxronik@gmail.com>
2021-10-21 14:29:20 +0500

    add polyfil

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,40 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.tsx',
-  devtool: 'inline-source-map',
+  mode: "development",
+  entry: ["babel-polyfill", "./src/index.tsx"],
+  devtool: "inline-source-map",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
-    publicPath: '/',
+    publicPath: "/",
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: [/\.jsx?$/, /\.tsx?$/],
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: ["babel-loader"],
       },
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
   devServer: {
-    contentBase: './dist',
+    contentBase: "./dist",
     port: 3000,
     historyApiFallback: true,
   },
-  plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })],
-}
+  plugins: [new HtmlWebpackPlugin({ template: "src/index.html" })],
+};
